578b3b1d86cefe7d2a50c263bbfe78f2e1dec0c5
John Richard Perez <46211205+Junorium@users.noreply.github.com>
2023-12-28 11:01:27 -0500

    Create and complete linear search

---
+++ SearchAlgorithms/linearsearch.py
@@ -0,0 +1,16 @@
+def linear_search(list, target):
+  """
+  Returns the index (numerical) of target if within list of elements, else return -1
+  """
+  for i in range(0, len(list):
+    if list[i] == target:
+  return -1
+
+try:
+  if __main__ == "__name__":
+    numbers = [1, 3, 5, 7, 9, 10]
+    target = 9
+
+    print(linear(numbers, target))
+except:
+  return -1
