0bfc97e73ca0142f93cce2e7deea97edb62727bc
John Richard Perez <46211205+Junorium@users.noreply.github.com>
2024-01-16 18:51:26 -0500

    Update merge_sort.py

--- SortingAlgorithms/merge_sort.py
+++ SortingAlgorithms/merge_sort.py
@@ -5,3 +5,13 @@ def merge_sort(list):
   - Merge repeatedly to produce sublist until only one sublist
   - Returns sorted list
   """
+
+  # if cannot be divided, automatically sorted; return
+  if len(list) == 1:
+    return list
+
+  left_half, right_half == #split list
+  left = merge_sort(left_half)
+  right = merge_sort(right_half)
+
+  return merge(left, right) # define merge function 
