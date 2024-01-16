d4ff02282044d89da178e51d3c3a6fd251c4102b
John Richard Perez <46211205+Junorium@users.noreply.github.com>
2024-01-16 18:48:34 -0500

    Create merge_sort.py and explain procedure

---
+++ SortingAlgorithms/merge_sort.py
@@ -0,0 +1,7 @@
+def merge_sort(list):
+  """
+  Sorts list in ascending order
+  - Recursively divide list into sublist containing a single node
+  - Merge repeatedly to produce sublist until only one sublist
+  - Returns sorted list
+  """
